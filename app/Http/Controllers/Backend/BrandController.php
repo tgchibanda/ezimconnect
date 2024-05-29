9363a1e153b30b187b4485696a63ad6db82700f1
Takunda Chibanda <tgchibanda@gmail.com>
2024-05-29 22:20:15 +1000

    Half done

---
+++ app/Http/Controllers/Backend/BrandController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Brand;
+
+class BrandController extends Controller
+{
+    public function AllBrand()
+    {
+        $brands = Brand::latest()->get();
+        return view('backend.brand.brand_all', compact('brands'));
+    }
+
+    public function AddBrand(){
+        return view('backend.brand.brand_add');
+   }
+}
--- resources/views/index/change_password.blade.php
+++ resources/views/index/change_password.blade.php
@@ -25,75 +25,68 @@ $title = $titleAndFolderPath['title'];
         </div>
     </div>
     <!--end breadcrumb-->
-    <div class="container">
-        <div class="main-body">
-            <div class="row">
 
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
+    <div class="card">
+        <div class="card-body">
 
-                            <form method="post" action="{{ route('update.password') }}">
-                                @csrf
+            <form method="post" action="{{ route('update.password') }}">
+                @csrf
 
-                                @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{session('status')}}
-                                </div>
-                                @elseif(session('error'))
-                                <div class="alert alert-danger" role="alert">
-                                    {{session('error')}}
-                                </div>
-                                @endif
-
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Old Password</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="password" name="old_password" class="form-control @error('old_password') is-invalid @enderror" id="current_password" placeholder="Old Password" />
-
-                                        @error('old_password')
-                                        <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">New Password</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="password" name="new_password" class="form-control @error('new_password') is-invalid @enderror" id="new_password" placeholder="New Password" />
-
-                                        @error('new_password')
-                                        <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Confirm New Password</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="password" name="new_password_confirmation" class="form-control" id="new_password_confirmation" placeholder="Confirm New Password" />
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-3"></div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
-                                    </div>
-                                </div>
-                        </div>
-
-                        </form>
-                    </div>
-
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{session('status')}}
                 </div>
-            </div>
+                @elseif(session('error'))
+                <div class="alert alert-danger" role="alert">
+                    {{session('error')}}
+                </div>
+                @endif
+
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Old Password</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="password" name="old_password" class="form-control @error('old_password') is-invalid @enderror" id="current_password" placeholder="Old Password" />
+
+                        @error('old_password')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">New Password</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="password" name="new_password" class="form-control @error('new_password') is-invalid @enderror" id="new_password" placeholder="New Password" />
+
+                        @error('new_password')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0">Confirm New Password</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="password" name="new_password_confirmation" class="form-control" id="new_password_confirmation" placeholder="Confirm New Password" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3"></div>
+                    <div class="col-sm-9 text-secondary">
+                        <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
+                    </div>
+                </div>
         </div>
+
+        </form>
     </div>
+
 </div>
+
 
 
 
