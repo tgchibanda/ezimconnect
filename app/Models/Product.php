b6afb41bb4a00cdfdb74f5a9c1418f1552385e0f
Takunda Chibanda <tgchibanda@gmail.com>
2024-06-01 19:29:34 +1000

    Section done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,7 +20,7 @@ class Product extends Model
 
     public function brand()
     {
-        return $this->belongsTo(Brand::class);
+        return $this->belongsTo(Brand::class,'brand_id','id');
     }
 
     public function category()
@@ -30,7 +30,7 @@ class Product extends Model
 
     public function subcategory()
     {
-        return $this->belongsTo(SubCategory::class);
+        return $this->belongsTo(SubCategory::class,'subcategory_id','id');
     }
 
 }
