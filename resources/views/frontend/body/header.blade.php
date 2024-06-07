2775c4257fbc893d72d64d5d58962cf40276ac4a
Takunda Chibanda <tgchibanda@gmail.com>
2024-06-07 18:03:53 +1000

    Update

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -58,19 +58,6 @@
                                 <form action="#">
                                     <select class="select-active">
                                         <option>Your Location</option>
-                                        <option>Alabama</option>
-                                        <option>Alaska</option>
-                                        <option>Arizona</option>
-                                        <option>Delaware</option>
-                                        <option>Florida</option>
-                                        <option>Georgia</option>
-                                        <option>Hawaii</option>
-                                        <option>Indiana</option>
-                                        <option>Maryland</option>
-                                        <option>Nevada</option>
-                                        <option>New Jersey</option>
-                                        <option>New Mexico</option>
-                                        <option>New York</option>
                                     </select>
                                 </form>
                             </div>
